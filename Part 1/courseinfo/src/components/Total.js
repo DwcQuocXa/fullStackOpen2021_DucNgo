ad4fd2f1ed04f97c98fe19c51bab259842212ffb
DucQuocXa <duc.ngogia310302@gmail.com>
2021-08-07 00:11:54 +0300

    courseinfo

--- Part 1/courseinfo/src/components/Total.js
+++ Part 1/courseinfo/src/components/Total.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-function Total() {
-  return <div></div>;
+function Total({ parts }) {
+  const partsExercise = parts.map((part) => part.exercises);
+  let sum = 0;
+  for (let i = 0; i < partsExercise.length; i++) {
+    sum += partsExercise[i];
+  }
+  return (
+    <div>
+      <h2>Number of exercises {sum}</h2>
+    </div>
+  );
 }
 
 export default Total;
