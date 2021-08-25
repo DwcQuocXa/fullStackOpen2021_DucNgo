ffc5b090acd3100e775c87fa82b11cb1ac1d7fa0
DucQuocXa <duc.ngogia310302@gmail.com>
2021-08-26 01:23:18 +0300

    Exercise 3.3-3.6

--- Part 3/index.js
+++ Part 3/index.js
@@ -1,34 +1,13 @@
 const { request, response } = require("express");
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+let persons = require("./Persons");
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-const PORT = process.env.PORT || 3001;
+app.use(express.json());
 
 app.get("/", (request, response) => {
-  response.send("<h1>Hello World!!!!</h1>");
+  response.status(200).send("<h1>Hello World!!!!</h1>");
 });
 
 app.get("/api/persons", (request, response) => {
@@ -42,4 +21,46 @@ app.get("/info", (request, response) => {
   `);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const personMatchesId = persons.find((person) => person.id === id);
+  if (personMatchesId) {
+    response.json(personMatchesId);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const newId =
+    persons.length > 0 ? Math.floor(Math.random() * 10000000 + 4) : 0;
+  return newId;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: "name or number missing" });
+  }
+
+  const duplicatedName = persons.find((person) => person.name === body.name);
+  if (duplicatedName) {
+    return response.status(400).json({ error: "name must be unique" });
+  }
+
+  const newPerson = new persons({
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  });
+});
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
