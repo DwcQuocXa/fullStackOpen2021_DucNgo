94030c4745334e6a740d148aec1e78010d3dfd04
DucQuocXa <duc.ngogia310302@gmail.com>
2021-09-15 16:49:13 +0300

    exercise 3.13-14

--- Part 3/index.js
+++ Part 3/index.js
@@ -2,7 +2,8 @@ const { request, response } = require("express");
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-let persons = require("./Persons");
+const mongoose = require("mongoose");
+const Person = require("./models/person");
 
 app.use(express.json());
 app.use(express.static("build"));
@@ -18,19 +19,21 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.status(200).json(persons);
+  Person.find({}).then((person) => {
+    response.json(person);
+  });
 });
 
-app.get("/info", (request, response) => {
-  const date = new Date();
-  response.status(200).send(`
-  <div><p>Phone book has info for ${persons.length} people </p><p>${date}</p></div>
-  `);
-});
+// app.get("/info", (request, response) => {
+//   const date = new Date();
+//   response.status(200).send(`
+//   <div><p>Phone book has info for ${persons.length} people </p><p>${date}</p></div>
+//   `);
+// });
 
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  const personMatchesId = persons.find((person) => person.id === id);
+  const personMatchesId = Person.find((person) => person.id === id);
   if (personMatchesId) {
     response.json(personMatchesId);
   } else {
@@ -38,12 +41,12 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
+// app.delete("/api/persons/:id", (request, response) => {
+//   const id = Number(request.params.id);
+//   persons = persons.filter((person) => person.id !== id);
 
-  response.status(204).end();
-});
+//   response.status(204).end();
+// });
 
 const generateId = () => {
   const newId =
@@ -69,5 +72,5 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT);
